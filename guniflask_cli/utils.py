51ddacb795f8ac0efacfafa95dc4ee602a03ad2b
jadbin <jadbin.com@hotmail.com>
2020-07-29 15:37:27 +0800

    remove unused utils

--- guniflask_cli/utils.py
+++ guniflask_cli/utils.py
@@ -47,21 +47,6 @@ def walk_files(path: str):
     return files
 
 
-def daemonize():
-    if os.fork():
-        os._exit(0)
-    os.setsid()
-    if os.fork():
-        os._exit(0)
-    os.umask(0o22)
-    os.closerange(0, 3)
-    fd_null = os.open(os.devnull, os.O_RDWR)
-    if fd_null != 0:
-        os.dup2(fd_null, 0)
-    os.dup2(fd_null, 1)
-    os.dup2(fd_null, 2)
-
-
 def pid_exists(pid):
     try:
         os.kill(pid, 0)
