488d7b7dbb1c1a9342c4e51d312a34e57f9bac4e
jadbin <jadbin.com@hotmail.com>
2020-07-29 22:33:03 +0800

    add manage command: restart

--- guniflask_cli/manage.py
+++ guniflask_cli/manage.py
@@ -187,6 +187,26 @@ class Start(Command):
             app.run()
 
 
+class Restart(Command):
+    @property
+    def name(self):
+        return 'restart'
+
+    @property
+    def short_desc(self):
+        return 'Restart application'
+
+    def run(self, args):
+        app = GunicornApplication()
+        pid = _read_pid(app.options)
+        if pid is None or not pid_exists(pid):
+            print('No application to stop')
+            self.exitcode = 1
+        else:
+            print('Sending HUB signal to master (pid: {})'.format(pid))
+            os.kill(pid, signal.SIGHUP)
+
+
 class Stop(Command):
     @property
     def name(self):
