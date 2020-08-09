28d8df656e5e5b7596ced3f3451410cbf6879f8e
jadbin <jadbin.com@hotmail.com>
2020-08-09 16:25:45 +0800

    add active profiles to help to locate PID file

--- guniflask_cli/commands/restart.py
+++ guniflask_cli/commands/restart.py
@@ -17,12 +17,30 @@ class Restart(Command):
     def short_desc(self):
         return 'Restart application'
 
+    def add_arguments(self, parser):
+        parser.add_argument('-p', '--active-profiles', dest='active_profiles', metavar='PROFILES',
+                            help='active profiles (comma-separated) which help to locate PID file')
+
     def run(self, args):
-        app = GunicornApplication()
-        pid = read_pid(app.options.get('pidfile'))
-        if pid is None or not pid_exists(pid):
-            print('No application to stop')
-            self.exitcode = 1
+        not_found = True
+        if args.active_profiles:
+            profile_list = [args.active_profiles]
         else:
-            print('Sending HUB signal to master (pid: {})'.format(pid))
-            os.kill(pid, signal.SIGHUP)
+            profile_list = ['prod', 'dev']
+        for p in profile_list:
+            os.environ['GUNIFLASK_ACTIVE_PROFILES'] = p
+            app = GunicornApplication()
+            pid = read_pid(app.options.get('pidfile'))
+            if self.send_hup(pid):
+                not_found = False
+                break
+        if not_found:
+            print('No application to restart')
+            self.exitcode = 1
+
+    def send_hup(self, pid):
+        if pid is None or not pid_exists(pid):
+            return False
+        print('Sending HUB signal to master (pid: {})'.format(pid))
+        os.kill(pid, signal.SIGHUP)
+        return True
--- guniflask_cli/commands/stop.py
+++ guniflask_cli/commands/stop.py
@@ -18,21 +18,39 @@ class Stop(Command):
     def short_desc(self):
         return 'Stop application'
 
+    def add_arguments(self, parser):
+        parser.add_argument('-p', '--active-profiles', dest='active_profiles', metavar='PROFILES',
+                            help='active profiles (comma-separated) which help to locate PID file')
+
     def run(self, args):
-        app = GunicornApplication()
-        pid = read_pid(app.options.get('pidfile'))
-        if pid is None or not pid_exists(pid):
-            print('No application to stop')
-            self.exitcode = 1
+        not_found = True
+        if args.active_profiles:
+            profile_list = [args.active_profiles]
         else:
-            print('kill {}'.format(pid))
-            os.kill(pid, signal.SIGTERM)
-            time.sleep(3)
-            try:
-                os.kill(pid, 0)
-            except OSError:
-                pass
-            else:
-                print('Application did not stop gracefully after 3 seconds')
-                print('kill -9 {}'.format(pid))
-                os.kill(pid, signal.SIGKILL)
+            profile_list = ['prod', 'dev']
+        for p in profile_list:
+            os.environ['GUNIFLASK_ACTIVE_PROFILES'] = p
+            app = GunicornApplication()
+            pid = read_pid(app.options.get('pidfile'))
+            if self.kill_pid(pid):
+                not_found = False
+                break
+        if not_found:
+            self.exitcode = 1
+            print('No application to stop')
+
+    def kill_pid(self, pid):
+        if pid is None or not pid_exists(pid):
+            return False
+        print('kill {}'.format(pid))
+        os.kill(pid, signal.SIGTERM)
+        time.sleep(3)
+        try:
+            os.kill(pid, 0)
+        except OSError:
+            pass
+        else:
+            print('Application did not stop gracefully after 3 seconds')
+            print('kill -9 {}'.format(pid))
+            os.kill(pid, signal.SIGKILL)
+        return True
