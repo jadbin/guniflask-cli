9e4841dc1ed9dea82535268b53172b462a11f5e8
jadbin <jadbin.com@hotmail.com>
2020-09-27 15:02:49 +0800

    update jwt config

--- guniflask_cli/templates/project/_project_name/config/jwt_config.py
+++ guniflask_cli/templates/project/_project_name/config/jwt_config.py
@@ -15,9 +15,8 @@ class JwtConfigurer(SecurityConfigurer):
         super().__init__()
         self.jwt_filter = None
         if isinstance(jwt, dict):
-            jwt_manager = JwtManager(**jwt)
-            settings['jwt_manager'] = jwt_manager
-            self.jwt_filter = JwtFilter(jwt_manager)
+            settings['jwt_manager'] = JwtManager(**jwt)
+            self.jwt_filter = JwtFilter()
 
     def configure(self, http: HttpSecurityBuilder):
         if self.jwt_filter:
@@ -25,12 +24,11 @@ class JwtConfigurer(SecurityConfigurer):
 
 
 class JwtFilter(RequestFilter):
-    def __init__(self, jwt_manager: JwtManager):
-        self.jwt_manager = jwt_manager
+    def __init__(self):
         self.token_extractor = BearerTokenExtractor()
 
     def before_request(self):
         auth = self.token_extractor.extract()
         if auth is not None:
-            user_auth = self.jwt_manager.authenticate(auth)
+            user_auth = jwt_manager.authenticate(auth)
             SecurityContext.set_authentication(user_auth)
